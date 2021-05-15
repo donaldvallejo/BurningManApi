45a8029c259d8bee35248e6be168b335b36e2d6f
donald vallejo <donny.vallejo@gmail.com>
2021-05-14 20:18:32 -0700

    added leroy

--- test/index.js
+++ test/index.js
@@ -2,11 +2,11 @@ const app = require("./../server");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const should = chai.should();
-
+const expect = chai.expect;
 chai.should();
 chai.use(chaiHttp);
 
-describe("Web Page", function() {
+describe("The Api", function() {
   it("Should have home page", function(done) {
     chai
       .request(app)
@@ -19,6 +19,21 @@ describe("Web Page", function() {
         return done(); 
       });
   });
+
+it("should create leroy", (done) => {
+  chai
+    .request(app)
+    .post("/new")
+    .send({
+      name: "leroooyyy jenkinnnnnsssss",
+    })
+    .end((err, res) => {
+      if (err) {
+        done(err);
+      }
+     done()
+    });
+})
 
   // // login
 // it("should be able to login", function(done) {
